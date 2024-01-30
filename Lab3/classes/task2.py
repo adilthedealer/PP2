f5b2335e94f1877d073c1e92186e199e7e0e4b5c
adilthedealer <a_salkimbayev@kbtu.kz>
2024-01-30 17:25:04 +0600

    second done

--- Lab3/classes/task2.py
+++ Lab3/classes/task2.py
@@ -1,2 +1,9 @@
 class Shape:
-    class 
+    def ar(self):
+        print(self.area)
+class Square(Shape):
+        def __init__(self, l):
+            self.area = l ** 2
+l = int(input())
+myarea = Square(l)
+myarea.ar()
