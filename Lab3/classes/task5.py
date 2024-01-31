541a45e4aaba0d994bc695d34001c690ccc7dc27
adilthedealer <a_salkimbayev@kbtu.kz>
2024-01-31 21:57:25 +0600

    5th done?

---
+++ Lab3/classes/task5.py
@@ -0,0 +1,26 @@
+class Account:
+    def __init__(self, owner, balance):
+        self.owner = owner.split()
+        self.balance = balance
+
+    def deposit(self, dep):
+        if dep > 0:
+            self.balance += dep
+        else:
+            print("______________")
+            print("Deposit error")
+
+    def withdraw(self, wth):
+        if wth < self.balance and wth > 0:
+            self.balance -= wth
+        else:
+            print("____________________")
+            print("Operation is denied")
+
+    def show(self):
+        print("_____________")
+        for i in self.owner:
+            print(i, end=" ")
+        print()
+        print(self.balance)
+
