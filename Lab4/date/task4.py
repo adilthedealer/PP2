35871a105d8a2ae3c7946404ca7653f08ff2e276
adilthedealer <a_salkimbayev@kbtu.kz>
2024-02-08 11:09:55 +0600

    solved task 4

--- Lab4/date/task4.py
+++ Lab4/date/task4.py
@@ -1,2 +1,10 @@
 import datetime
 
+mlist = list(map(int, input().split()))
+year1, month1, day1 = mlist[2], mlist[1], mlist[0]
+year2, month2, day2 = mlist[5], mlist[4], mlist[3]
+j = datetime.date(year1, month1, day1)
+k = datetime.date(year2, month2, day2)
+date1 = k - j
+ans = date1.total_seconds()
+print(ans)
