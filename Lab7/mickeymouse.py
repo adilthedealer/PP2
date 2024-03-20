cdf2f5484c687e7bc4a8bee4575aa1ee1eadb7c0
adilthedealer <a_salkimbayev@kbtu.kz>
2024-03-20 13:25:45 +0500

    changed the mickey mouse clock so that the conditions are properly met

--- Lab7/mickeymouse.py
+++ Lab7/mickeymouse.py
@@ -12,8 +12,8 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Mickey Mouse Clock")
 
 clock_face = pygame.image.load("Lab7/mmclock/mainclock.png").convert_alpha()
-hour_hand = pygame.image.load("Lab7/mmclock/rightarm.png").convert_alpha()
-minute_hand = pygame.image.load("Lab7/mmclock/leftarm.png").convert_alpha()
+minute_hand = pygame.image.load("Lab7/mmclock/rightarm.png").convert_alpha()
+second_hand = pygame.image.load("Lab7/mmclock/leftarm.png").convert_alpha()
 
 
 clock_center = (screen_width // 2, screen_height // 2)
@@ -38,21 +38,23 @@ while True:
                 sys.exit()
 
 
-    current_time = datetime.datetime.now() - datetime.timedelta(minutes=15)
-    hour = current_time.hour % 12
+    current_time = datetime.datetime.now() - datetime.timedelta(minutes = 7) - datetime.timedelta(seconds=13)
+    hour = current_time.hour
     minute = current_time.minute
+    second = current_time.second
 
     screen.fill((255, 255, 255))
 
     screen.blit(clock_face, (0, 0))
 
-    hour_hand_rotated, hour_hand_rect = rotate(hour_hand, -(hour % 12) * 30 - 90)
-    screen.blit(hour_hand_rotated, hour_hand_rect)
-
-    minute_hand_rotated, minute_hand_rect = rotate(minute_hand, -minute * 6 - 90)
+    minute_angle = -minute * 6 - (second / 10) - 90
+    minute_hand_rotated, minute_hand_rect = rotate(minute_hand, minute_angle)
     screen.blit(minute_hand_rotated, minute_hand_rect)
+
+    second_angle = -second * 6 - 90
+    second_hand_rotated, second_hand_rect = rotate(second_hand, second_angle)
+    screen.blit(second_hand_rotated, second_hand_rect)
 
     pygame.display.flip()
 
-    clock.tick(60)
-
+    clock.tick(60)  # Update every second
